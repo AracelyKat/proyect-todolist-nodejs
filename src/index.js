6ff77f1b12fbc03c26b7ffa129a9b0bb28f06b4f
Aracely Katy <aracelykeyty@gmail.com>
2025-11-06 10:22:10 -0900

    crud task

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.routes.js";
 import categoryRoutes from "./routes/category.routes.js";
 import tagRoutes from "./routes/tag.routes.js";
+import taskRoutes from "./routes/task.routes.js";
 
 dotenv.config();
 const app = express();
@@ -11,6 +12,7 @@ app.get("/", (req, res) => res.send("Servidor Express funcionando"));
 app.use("/api/users", userRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/tags", tagRoutes);
+app.use("/api/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
