f5a70cd858084f73e8e9757dfc4c03aafbfeb730
Aracely Katy <aracelykeyty@gmail.com>
2025-11-13 05:51:05 -0900

    correccion en Tasks.controllers

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const db = await mysql.createConnection({
+export const db = await mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASS || "",
